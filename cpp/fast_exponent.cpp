5fdd03edaefdbcddcdc5cd6d7b7108c851b1146d
Tony <buddy90_lost@yahoo.co.id>
2017-10-25 09:34:28 +0700

    Update fast_exponent.cpp

--- cpp/fast_exponent.cpp
+++ cpp/fast_exponent.cpp
@@ -5,7 +5,7 @@ typedef long long ll;
 const ll mod = 1e10;
 const ll N = 61;
 
-ll a,b,r,c,d,e,f, p[N+2], n[N+2],i;
+ll a,b,r,c,p[N+2], n[N+2],i;
 
 int main(){
 	scanf("%lld %lld", &a, &b);
@@ -17,13 +17,9 @@ int main(){
 	}
 	for(n[0]=a, i=1;i<N;i++)
 		n[i] = (n[i-1]*n[i-1])%mod;
-	for(r=1,i=0;i<N;i++){
-		if(p[i]){
-			printf("%lld\n", i);
-			r*=n[i];
-			r%=mod;
-		}
-	}
+	for(r=1,i=0;i<N;i++)
+		if(p[i])
+			r=(r*n[i]) % mod;
 	printf("%lld\n", r);
 	return 0;
 }
