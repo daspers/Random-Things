318ba1ed9706f94df40a448943bc2b191aa6fc6f
Tony <buddy90_lost@yahoo.co.id>
2018-05-16 22:37:36 +0700

    Create fft2.cpp

---
+++ cpp/fft2.cpp
@@ -0,0 +1,80 @@
+/* ITTERATIVE VERSION (fast) */
+/* e-maxx */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pii pair<int, int>
+#define Nmax 300000
+
+typedef long long ll;
+typedef complex<double> cd;
+
+const ll mod = 1e9+7;
+const double pi = acos(-1);
+
+int reverse(int num, int lg){
+	int res = 0;
+	for(int i=0;i<lg;++i){
+		if(num & (1<<i))
+			res |= 1<<(lg-1-i);
+	}
+	return res;
+}
+
+void fft(vector<cd> &a, bool invert){
+	int n = a.size();
+	int lg = 0;
+	while((1<<lg) < n)
+		++lg;
+	for(int i=0;i<n;++i){
+		if(i<reverse(i, lg))
+			swap(a[i], a[reverse(i, lg)]);
+	}
+	int neg = invert?-1:1;
+	for(int len=2;len<=n;len<<=1){
+		double ang = 2.0*pi/len*neg;
+		cd wn(cos(ang), sin(ang));
+		for(int i=0;i<n;i+=len){
+			cd w(1);
+			for(int j=0;j<len/2;++j){
+				cd u=a[i+j], v=a[i+j+len/2]*w;
+				a[i+j] = u+v;
+				a[i+j+len/2] = u-v;
+				w *= wn;
+			}
+		}
+	}
+	if(invert)
+		for(cd & tmp : a)
+			tmp/=n;
+}
+
+vector<int> multiply(const vector<int> &a, const vector<int> &b){
+	vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+	int sz = 1;
+	while(sz < a.size()+b.size())
+		sz <<= 1;
+	fa.resize(sz);
+	fb.resize(sz);
+	fft(fa, false);
+	fft(fb, false);
+	
+	for(int i=0;i<sz;++i)
+		fa[i] *= fb[i];
+	fft(fa, true);
+	
+	vector<int> res;
+	for(int i=0;i<sz;++i){
+		res.pb(round(fa[i].real()));	//easy
+	}
+	return res;
+}
+
+int main(){
+	return 0;
+}
